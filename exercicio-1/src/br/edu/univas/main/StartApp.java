db4d0ae6012d7b15110d09dedc8ae7f8db2d3e2a
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-05-12 21:43:55 -0300

    Add StartApp

---
+++ exercicio-1/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,98 @@
+package br.edu.univas.main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import br.edu.univas.vo.CLT;
+import br.edu.univas.vo.Estagiario;
+import br.edu.univas.vo.Funcionario;
+import br.edu.univas.vo.Vendedor;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		//jeito 1
+		StartApp app = new StartApp();
+		app.initialize();
+		
+		//jeito 2
+		//new StartApp().initialize();
+	}
+	
+	public void initialize() {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Funcionario> funcionarios = new ArrayList<>();
+		int option = 0;
+		
+		do {
+			printMenu();
+			option = scanner.nextInt();
+			
+			if (option == 1) {
+				cadastrar(scanner, funcionarios);
+				
+			} else if (option == 2) {
+				listar(funcionarios);				
+				
+			} else if (option != 9) {
+				System.out.println("Opção inválida!");
+				
+			} else {
+				System.out.println("Bye bye");
+			}		
+			
+		} while(option != 9);
+		
+		scanner.close();
+	}
+	
+	private void cadastrar(Scanner scanner, ArrayList<Funcionario> funcionarios) {
+		printSubMenu();
+		int option = scanner.nextInt();
+		Funcionario funcionario = null;
+		
+		System.out.println("Digite o salario base:");
+		float salarioBase = scanner.nextFloat();
+		
+		if (option == 1) {
+			funcionario = new CLT(salarioBase);
+			funcionarios.add(funcionario);
+			
+		} else if (option == 2) {
+			System.out.println("Digite o valor pago por hora:");
+			float valorHora = scanner.nextFloat();
+			funcionario = new Estagiario(salarioBase, valorHora);
+			funcionarios.add(funcionario);
+			
+		} else if (option == 3) {
+			System.out.println("Digite o valor da comissão:");
+			float comissao = scanner.nextFloat();
+			funcionario = new Vendedor(salarioBase, comissao);
+			funcionarios.add(funcionario);
+		}
+	}
+	
+	private void listar(ArrayList<Funcionario> funcionarios) {
+		System.out.println("::::Cadastro de Funcionário::::");
+				
+		for (Funcionario funcionario : funcionarios) {
+			System.out.println(funcionario);
+		}
+	}
+	
+	private void printMenu() {
+		System.out.println("::::Cadastro de Funcionário::::");
+		System.out.println("1 - Cadastrar Novo Funcionário");
+		System.out.println("2 - Listar todos os Funcionários");
+		System.out.println("9 - Sair");
+	}
+	
+	private void printSubMenu() {
+		System.out.println("::::Cadastro de Funcionário::::");
+		System.out.println("1 - CLT");
+		System.out.println("2 - Estagiário");
+		System.out.println("3 - Vendedor");
+		System.out.println("9 - Voltar");
+	}
+
+}
